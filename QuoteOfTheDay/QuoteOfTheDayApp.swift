78e8f3d782f188a0171e12489945225dfafed976
José Luís <josegalinha98@gmail.com>
2025-05-24 11:47:07 +0100

    Initial Commit

---
+++ QuoteOfTheDay/QuoteOfTheDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteOfTheDayApp.swift
+//  QuoteOfTheDay
+//
+//  Created by José Luís on 24/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuoteOfTheDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuoteOfTheDayTests/QuoteOfTheDayTests.swift
@@ -0,0 +1,16 @@
+//
+//  QuoteOfTheDayTests.swift
+//  QuoteOfTheDayTests
+//
+//  Created by José Luís on 24/05/2025.
+//
+
+import Testing
+
+struct QuoteOfTheDayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
